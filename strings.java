9fac0b9396eaddbbd142fcba6a8c69bdddec5a1b
Aryan Dwivedi <94976692+RND2002@users.noreply.github.com>
2022-01-16 20:17:10 +0530

    Add files via upload

--- strings.java
+++ strings.java
@@ -1,13 +1,17 @@
 import java.util.*;
 public class strings {
     public static void main(String args[]){
-        // String firstname= "Tony";
-        // String lastname="Stark";
-        // String fullname="Tony stark";
+         String firstname= "Tony";
+         String lastname="Stark";
+         String fullname="Tony stark";
         // System.out.println(fullname);
-        Scanner sc=new Scanner(System.in);
-        String name= sc.nextLine();
-        System.out.println("your name is :" + name);
+        // Scanner sc=new Scanner(System.in);
+        // String name= sc.nextLine();
+        // System.out.println("your name is :" + name);
+        System.out.println(fullname.length());
+        for(int i=0;i<fullname.length();i++){
+            System.out.println(fullname.charAt(i));
+        }
         
     }
     
