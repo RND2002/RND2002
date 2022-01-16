efa19835b8a2d60a0559c7caff92bfa8c9a9135b
Aryan Dwivedi <94976692+RND2002@users.noreply.github.com>
2022-01-16 20:08:02 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class strings {
+    public static void main(String args[]){
+        // String firstname= "Tony";
+        // String lastname="Stark";
+        // String fullname="Tony stark";
+        // System.out.println(fullname);
+        Scanner sc=new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.println("your name is :" + name);
+        
+    }
+    
+}
